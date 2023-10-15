74e07d20467a951dadac5383748a29e21b212289
aiantorres <aiantorres@users.noreply.github.com>
2023-10-16 00:39:25 +0200

    v1.2

--- app_streamlit.py
+++ app_streamlit.py
@@ -19,8 +19,14 @@ logo="./img/temperatura.png"
 st.sidebar.image(logo, width=150, use_column_width="auto")
 st.sidebar.header('   Datos ingresados por el usuario',divider='rainbow')
 
-def input_parameter():
-    
-    
-                
+#def input_parameter():
+#   evspsbl = st.sidebar.slider()
+data=pd.read_excel(r"C:\Users\usuario\OneDrive\Documents\GitHub\temperatureprediction\data\raw\OrlandoStation.xls",index_col=0)  
+
+
+st.map(data,
+    latitude='POINT_Y',
+    longitude='POINT_X',
+    size='20',
+    color='#0044ff')
                    
